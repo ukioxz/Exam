8daf087fda5ef1291ce772072f96a70bb037ddb1
мрачный чудик <72039145+ukioxz@users.noreply.github.com>
2021-04-14 16:11:34 +0300

    aaa

--- composition.js
+++ composition.js
@@ -8,9 +8,20 @@ const upperCase = str => str.toUpperCase();
 const exclaim = str => `${str}!!!!!!`;
 const repeat = str =>`${str}`.repeat(3);
 
+const upperCase1 = str1 => str1.toUpperCase();
+const exclaim1 = str1 => `${str1}!!!!!!`;
+const repeat1 = str1 =>`${str1}`.repeat(3);
+
+
+
+const str = "tired"
+const str1 = "aa";
+
 const compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x);
-const withCompose = compose(repeat, exclaim, upperCase);
+const withCompose = compose(repeat1, exclaim1, upperCase1);
 
-const str = "tired";
+const pipe = (...fns) => x => fns.reduce((acc, fn) => fn(acc), x);
+const withPipe = pipe(repeat, exclaim, upperCase);
 
-console.log(`${withCompose(str)}`);
+console.log(`${withCompose(str)}`); //TIRED!!!!!!TIRED!!!!!!TIRED!!!!!!
+console.log(`${withPipe(str1)}`); //AAAAAA!!!!!!
