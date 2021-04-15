f4d8d9b0efb144b07a7161979e43f6a16bc0d4cf
мрачный чудик <72039145+ukioxz@users.noreply.github.com>
2021-04-15 23:18:31 +0300

    Add files via upload

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function getResult(x) {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -6,7 +8,7 @@ function getResult(x) {
   });
 }
 
-async function add1(x) {
+async function add1(x) {         //преостанавливает на 3 сек для первого await и еще на 3 сек для второго
   const a = await getResult(10);
   const b = await getResult(4);
   return x + a + b;
@@ -15,3 +17,39 @@ async function add1(x) {
 add1(5).then(v => {
   console.log(v);
 });
+
+
+
+
+
+
+const obj = {
+  name: ' Marcus Aurelius ',
+  async trim() {
+    return this.name.trim();
+  }
+};
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static async of(name) {
+    return await new Person(name); // оборачиваем в промис 
+  }
+
+  async trim() {
+    return this.name.trim();
+  }
+}
+
+const person = new Person(' Marcus Aurelius ');
+
+(async () => {
+
+
+  console.log('await obj.repeat() =', await obj.trim());
+  console.log('await person.repeat() =', await person.trim());
+
+})();
