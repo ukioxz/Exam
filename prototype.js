f6152337cad80008ad980d696dd2da3f9846d37c
мрачный чудик <72039145+ukioxz@users.noreply.github.com>
2021-04-17 16:37:02 +0300

    SM1

--- prototype.js
+++ prototype.js
@@ -31,3 +31,62 @@ p1.move(-1, 3);
 console.log(p1);
 console.log(p1.toString());
 console.log(p1 + '');
+
+
+
+//через классы
+
+'use strict';
+
+class Result {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(x, y) {
+    this.x += x+2;
+    this.y += y+2;
+  }
+
+  toString() {
+    return `${this.x}, ${this.y}`;
+  }
+
+  static from(obj) {
+    const {x, y} = obj;
+    return new Result(x, y);
+  }
+}
+
+const p1 = new Result(7, 4);
+p1.plus(-2, 3);
+
+console.log(p1);
+console.log(p1.toString());
+console.log(p1 + '');
+
+
+
+
+//через bind
+
+'use strict';
+
+function getNew(x, y) {
+  this.x += x;
+  this.y += y;
+}
+
+function toString() {
+  return `${this.x}, ${this.y}`;
+}
+
+const p1 = {x: 6, y: 10};
+const p1get = getNew.bind(p1);       // создаем метод и через bind привязываем к p1
+const p1toString = toString.bind(p1);
+p1get(-5, 7);
+
+console.log(p1);
+console.log(p1toString());
+console.log(p1 + '');
