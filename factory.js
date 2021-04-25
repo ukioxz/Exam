ecf8ca7897cdf1c41c8520fa870390d60c1684f5
мрачный чудик <72039145+ukioxz@users.noreply.github.com>
2021-04-25 18:39:35 +0300

    factory

--- factory.js
+++ factory.js
@@ -1,5 +1,7 @@
 //фабрики объектов
 
+'use strict';
+
 const useFactory1 = (name, host, age) => ({
   name, host, age
 });
@@ -14,3 +16,71 @@ const user2 = useFactory2("Charly", "Pete", 3);
 
 console.log(user1);
 console.log(user2);
+
+
+
+
+'use strict';
+
+const colors = {
+  warning: '\x1b[1;36m',
+  info: '\x1b[1;35m'
+};
+
+const logger = (level = 'info') => {
+  const color = colors[level];
+  return s => {
+    const date = new Date().toISOString();
+    console.log(color + date + '\t' + s);
+  };
+};
+
+const warning = logger('warning');
+warning('Charly');
+
+const info = logger('info');
+info('Charmin');
+
+
+
+
+//фабричные методы
+
+'use strict';
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  static factory(name) {
+    return new Cat(name);
+  }
+}
+
+//const cat1 = new Cat('Charly');
+//console.dir({ cat1});
+
+const cat2 = Cat.factory('Charmin');
+console.dir({ cat2 });
+
+
+
+
+'use strict';
+
+class Cat {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const factorify = Category => (...args) => new Category(...args); //из класса делаем фабрику
+
+
+/*const cat1 = new Cat('Charly', 3); //инстанциирование через new
+console.dir({ cat1 });*/
+
+const catFactory = factorify(Cat);// инстациирование
+const cat1 = catFactory('Charly', 3);
+console.dir({ cat1 });
